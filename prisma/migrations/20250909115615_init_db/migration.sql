7ab1dad023a8a44d4267197502da22ae4380f328
vatsal prajapati <prajapativatsal555@gmail.com>
2025-09-09 17:58:22 +0530

    feat: implement database schema and migration for workflow management

---
+++ prisma/migrations/20250909115615_init_db/migration.sql
@@ -0,0 +1,112 @@
+-- CreateTable
+CREATE TABLE "public"."Workflow" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "definition" TEXT NOT NULL,
+    "executionPlan" TEXT,
+    "creditsCost" INTEGER NOT NULL DEFAULT 0,
+    "status" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "lastRunAt" TIMESTAMP(3),
+    "lastRunId" TEXT,
+    "lastRunStatus" TEXT,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "cron" TEXT,
+    "nextRunAt" TIMESTAMP(3),
+
+    CONSTRAINT "Workflow_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."WorkflowExecution" (
+    "id" TEXT NOT NULL,
+    "workflowId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "trigger" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "startedAt" TIMESTAMP(3),
+    "completedAt" TIMESTAMP(3),
+    "creditsConsumed" INTEGER NOT NULL DEFAULT 0,
+    "definition" TEXT NOT NULL DEFAULT '{}',
+
+    CONSTRAINT "WorkflowExecution_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."ExecutionPhase" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "number" INTEGER NOT NULL,
+    "node" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "startedAt" TIMESTAMP(3),
+    "completedAt" TIMESTAMP(3),
+    "inputs" TEXT,
+    "outputs" TEXT,
+    "creditsConsumed" INTEGER,
+    "workflowExecutionId" TEXT NOT NULL,
+
+    CONSTRAINT "ExecutionPhase_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."ExecutionLog" (
+    "id" TEXT NOT NULL,
+    "logLevel" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "executionPhaseId" TEXT NOT NULL,
+
+    CONSTRAINT "ExecutionLog_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."UserBalance" (
+    "userId" TEXT NOT NULL,
+    "credits" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "UserBalance_pkey" PRIMARY KEY ("userId")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Credential" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Credential_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."UserPurchase" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "stripeId" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "currency" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "UserPurchase_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Workflow_name_userId_key" ON "public"."Workflow"("name", "userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Credential_userId_name_key" ON "public"."Credential"("userId", "name");
+
+-- AddForeignKey
+ALTER TABLE "public"."WorkflowExecution" ADD CONSTRAINT "WorkflowExecution_workflowId_fkey" FOREIGN KEY ("workflowId") REFERENCES "public"."Workflow"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."ExecutionPhase" ADD CONSTRAINT "ExecutionPhase_workflowExecutionId_fkey" FOREIGN KEY ("workflowExecutionId") REFERENCES "public"."WorkflowExecution"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."ExecutionLog" ADD CONSTRAINT "ExecutionLog_executionPhaseId_fkey" FOREIGN KEY ("executionPhaseId") REFERENCES "public"."ExecutionPhase"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ src/app/workflows/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex-1 flex flex-col h-full">
+      <div className="flex justify-between">
+        <div className="flex flex-col">
+          <h1 className="text-3xl font-bold">Workflows</h1>
+          <p className="text-muted-foreground">Manage your workflows</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
