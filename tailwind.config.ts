c94441adda934e820596137131f83fb9a9271565
vatsal prajapati <prajapativatsal555@gmail.com>
2025-09-09 15:41:54 +0530

    refactor: update package dependencies and improve CSS variables
    - Removed "@tailwindcss/cli" from package.json.
    - Adjusted CSS variables for radius and chart colors in globals.css and style.css for better consistency.
    - Updated Tailwind CSS configuration to simplify paths for content scanning.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,11 @@ import type { Config } from "tailwindcss";
 const config = {
   darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{ts,tsx}",
-    "./src/components/**/*.{ts,tsx}",
-    "./src/app/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
-    "./src/lib/**/*.{ts,tsx}",
+    "./lib/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
